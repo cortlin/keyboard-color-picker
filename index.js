1e542f9f4aa2f12cf7ee8a9a42db6df8b3afd727
Cortlin <cortlin.sawyer@gmail.com>
2020-09-21 16:17:50 -0500

    stuff

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 console.log("It WOrks!");
 
-document.querySelector("#submit").addEventListener("click", function(event) {
-    event.preventDefault();
-}, false);
+let colorChange;
+const defaultColor = "#2fdaa7";
 
+window.addEventListener("load", startup, false);
 
+function startup() {
+    colorChange = document.querySelector("#picker");
+    colorChange.value = defaultColor;
+    colorChange.addEventListener("input", updateFirst, false);
+    colorChange.addEventListener("change", updateAll, false);
+    colorChange.select();
+};
 
+function updateFirst(event) {
+    var p = document.querySelector(".key1");
+  
+    if (p) {
+      p.style.backgroundColor = event.target.value;
+    }
+  }
 
+function updateAll(event) {
+    document.querySelectorAll(".key1").forEach(function(p) {
+        p.style.backgroundColor = event.target.value;
+    });
+}
