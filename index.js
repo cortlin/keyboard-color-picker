7d68facf4e9b9e41966c03d43535550aba777579
Cortlin <cortlin.sawyer@gmail.com>
2020-09-22 15:20:28 -0500

    more stuff

--- index.js
+++ index.js
@@ -6,23 +6,36 @@ const defaultColor = "#2fdaa7";
 window.addEventListener("load", startup, false);
 
 function startup() {
-    colorChange = document.querySelector("#picker");
-    colorChange.value = defaultColor;
-    colorChange.addEventListener("input", updateFirst, false);
-    colorChange.addEventListener("change", updateAll, false);
-    colorChange.select();
-};
+  colorChange = document.querySelector("#picker");
+  colorChange.value = defaultColor;
+  colorChange.addEventListener("input", updateFirst, false);
+  colorChange.addEventListener("change", updateAll, false);
+  colorChange.select();
 
-function updateFirst(event) {
-    var p = document.querySelector(".key1");
-  
+  function updateFirst(event) {
+    var p = document.querySelector(".selected");
+
     if (p) {
       p.style.backgroundColor = event.target.value;
     }
   }
 
-function updateAll(event) {
-    document.querySelectorAll(".key1").forEach(function(p) {
-        p.style.backgroundColor = event.target.value;
+  function updateAll(event) {
+    document.querySelectorAll(".selected").forEach(function (p) {
+      p.style.backgroundColor = event.target.value;
+      p.classList.remove('selected');
     });
+  }
 }
+
+
+const keys = document.querySelectorAll('.key1');
+
+for (const key of keys) {
+  key.addEventListener('click', function() {
+    this.classList.add('selected');
+  })
+}
+
+
+
